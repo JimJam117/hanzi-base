42f31c5081f554ea42dd6d94fac2867a796ff401
jimjam117 <jamessparrow101@gmail.com>
2020-02-20 03:46:08 +0000

    Added custom message for radical not found page

--- resources/views/character/notfound.blade.php
+++ resources/views/character/notfound.blade.php
@@ -9,13 +9,17 @@ Character "{{$char}}" Not Found
 @section('main')
 <div class="notfound">
     <h1>很抱歉！</h1>
-    @isset($char)
-    <h2>The character you were looking "{{$char}}" for couldn't be found</h2>
-    @else
-    <h2>The character or page you were looking for couldn't be found</h2>  
-    @endisset
 
-<p>If you suspect that this is an error, please contact the admin jamesparrow101@googlemail.com</p>
+    @if(isset($notRadical) && isset($char))
+        <h2>The radical you were looking for "{{$char}}" couldn't be found</h2>
+    @elseif (isset($char))
+        <h2>The character you were looking for "{{$char}}" couldn't be found</h2>
+    @else
+        <h2>The character or page you were looking for couldn't be found</h2>  
+    @endif
+
+    <p>If you suspect that this is an error, please contact the admin jamesparrow101@googlemail.com</p>
+    
 </div>
 @endsection
    
